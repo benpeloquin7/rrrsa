9c802cd028810a55bae9de23aa69484fe2b4549a
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-02-16 21:31:49 -0800

    first data validation checking

--- R/utils.R
+++ R/utils.R
@@ -15,7 +15,7 @@ normVec <- function(v) {
 }
 
 #' validateDims
-#' Compare dimensions of two matrices
+#' Compare dimensions of two matrices or vectors
 #' @param m1, matrix 1
 #' @param m2, matrix 2
 #' @keywords safety_checks
