06b6a22ea305a52f6be713d5e408ddf590eb543b
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-02-19 14:01:51 -0800

    updating additional files so MF can pull and run

---
+++ R/practiceData.R
@@ -0,0 +1,9 @@
+#' Practice data from Peloquin \& Frank (2015)
+#'
+#' A test data set of four variables \code{scale}, \code{degree},
+#' \code{stars}, \code{semantics}
+#' @name practiceData
+#' @docType data
+#' @keywords data
+#' @usage data(practiceData)
+NULL
