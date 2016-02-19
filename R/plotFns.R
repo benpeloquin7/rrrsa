dcbf4428da8217ea8f6dde565fe55da7bee17156
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-02-19 12:54:07 -0800

    basic semantics plotting working

--- R/plotFns.R
+++ R/plotFns.R
@@ -1,8 +1,21 @@
 #' Plot literal listener semantics
 #' Compare dimensions of two matrices or vectors
 #' @param m1, matrix 1
-#' @param m2, matrix 2
-#' @keywords safety_checks
+#' @param processedData, data frame after proceessing (from calling processData())
+#' @keywords plotting
 #' @examples
 #'
+#' processedData <- processData(practiceData)
+#' plotSemantics(processedData)
 #'
+plotSemantics <- function(processedData) {
+  plotData <- processedData$originalData
+  labels <- processedData$labels
+
+  ggplot2::ggplot(plotData,
+                  ggplot2::aes(x = plotData[[labels[["quantity"]]]],
+                               y = plotData[[labels[["semantics"]]]],
+                               col = plotData[[labels[["item"]]]])) +
+    ggplot2::geom_line() +
+    ggplot2::facet_wrap(as.formula(paste("~", labels[["group"]])))
+}
