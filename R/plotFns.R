1c068720f62130df77245a82693eb62099a9f86f
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-02-19 12:01:41 -0800

    initial plotFns file, no implementations yet

---
+++ R/plotFns.R
@@ -0,0 +1,8 @@
+#' Plot literal listener semantics
+#' Compare dimensions of two matrices or vectors
+#' @param m1, matrix 1
+#' @param m2, matrix 2
+#' @keywords safety_checks
+#' @examples
+#'
+#'
