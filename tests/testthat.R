8aa9227904a457753979259fc5b73eeec72c9394
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-03-05 21:26:42 -0800

    updated documentation

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,12 +3,13 @@ library(rrrsa)
 
 test_check("rrrsa")
 
-# informativity()
-# ----------------
+#' unit tests
+#'
 test_that("informativity returns valid value", {
   expect_equal(informativity(0), 0)
   expect_equal(informativity(0.3), 0.3)
 })
+
 test_that("informativity passed correct value", {
   expect_error(informativity(-1.5),)
   expect_error(informativity(2),)
