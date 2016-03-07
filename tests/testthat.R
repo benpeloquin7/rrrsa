143168f5b440e6a14802c28a697758cf60f6965b
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-03-07 13:44:21 -0800

    basic testing for rsa.informativity and rsa.utility

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,14 +3,48 @@ library(rrrsa)
 
 test_check("rrrsa")
 
-#' unit tests
-#'
+
+## rsa.informativity()
+## -------------------
+## Informativity returns correct value
 test_that("informativity returns valid value", {
-  expect_equal(informativity(0), 0)
-  expect_equal(informativity(0.3), 0.3)
+  expect_equal(rsa.informativity(0), 0)
+  expect_equal(rsa.informativity(0.3), 0.3)
+  expect_equal(rsa.informativity(0.0), 0.0)
+  expect_equal(rsa.informativity(1.0), 1)
+})
+## Informativity passed correct values
+test_that("informativity passed valid value", {
+
+  expect_error(rsa.informativity(m_u = -1.5))    #! no negative vals
+  expect_error(rsa.informativity(m_u = 2))       #! values must be between [0, 1]
+  expect_error(rsa.informativity(m_u = 0.5, alpha = 0),
+               "Invalid alpha, must be a positive, non-zero numerical expression")  #! alpha values must be (0, +inf)
+  expect_error(rsa.informativity(m_u = 0.5, alpha = -1),
+               "Invalid alpha, must be a positive, non-zero numerical expression")  #! alpha values must be (0, +inf)
+})
+debug(rsa.informativity)
+undebug(rsa.informativity)
+
+## rsa.utility()
+## -------------
+test_that("utilty returns valid value", {
+  items1 <- c(0.5, 0.5)
+  items2 <- c(0.2, 0.8)
+  costs <- c((-1 * (-log(0.5) + log(0.2))), (-1 * (-log(0.5) + log(0.8))))
+  expect_equal(rsa.utility(items = items1), items1)
+  expect_equal(rsa.utility(items = items2), items2)
+  expect_equal(rsa.utility(items = items2, costs = costs), items1)
 })
 
-test_that("informativity passed correct value", {
-  expect_error(informativity(-1.5),)
-  expect_error(informativity(2),)
+test_that("utility passed valid value", {
+  items1 <- c("apple", "banana", "pear")
+  costs1 <- seq(1,5)
+  costs2 <- seq(1,length(items1))
+  expect_error(rsa.utility(items = items1, costs = costs1), #! items and costs vectors must be same length
+               "Item and cost dimensions do not match")
+  expect_error(rsa.utility(items = items1, costs = costs2, alpha = costs1), #! alpha should be length 1
+               "Alpha should be a single numerical expression")
 })
+debug(rsa.utility)
+undebug(rsa.utility)
