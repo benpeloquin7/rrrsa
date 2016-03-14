be0bfeffdb12e480f9d18568001da97cf4826fd0
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-03-14 10:30:04 -0700

    updated rrrsa.r descriptions (help page)

--- R/rrrsa.R
+++ R/rrrsa.R
@@ -1,12 +1,27 @@
-#' rrrsa package for running Rational Speech Act models
+#' run Rational Speech Act (RSA) models
 #'
+#' rrrsa provides tools for running RSA models.
+#' These tools are grouped below for basic RSA reasoning functionality
+#' as well as handling data frames.
 #'
-#' for functions \code{link{rsa.reason}} and \code{link{rsa.informativity}}.
-#' @seealso \code{\link{rsa.reason}}
-#' @seealso \code{link{rsa.informativity}}
+#' Reasoning
+#'
+#' rrrsa's reasoning is broken down into model components made available to users.
+#' Calculating informativity \code{\link{rsa.informativity}}, calculating
+#' speaker utility \code{\link{rsa.utility}}, running one full RSA recursion
+#' \code{\link{rsa.fullRecursion}}, running multiple recursions \code{\link{rsa.reason}}.
+#'
+#' Data frames and tuning hypeparameters
+#'
+#' rrrsa provides easy functionality for data in data frames with \code{\link{rsa.runDf}},
+#' as well as for tuning hyperparamters \code{\link{rsa.tuneDepthAlpha}}
+#'
 #' @docType package
-#' @references Frank \& Goodman (2012)
-#' \url{http://langcog.stanford.edu/papers_new/frank-2012-science.pdf}
+#'
+#' @references Frank & Goodman (2012); Goodman & Stuhmuller, (2014); Peloquin & Frank (2016)
+#'
+#' \url{https://github.com/benpeloquin7/rrrsa}
+#'
 #' @name rrrsa
 #'
 NULL
