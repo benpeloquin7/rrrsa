4d7340e1066e75fe4aa091b54cc8d9129e0bcc6d
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-03-14 10:57:35 -0700

    updated Roxygen comments for data

--- R/peloquinFrank_4Alts.R
+++ R/peloquinFrank_4Alts.R
@@ -1,7 +1,24 @@
 #' Four alternatives data from Peloquin and Frank 2016
 #'
+#' This data set contains empirical literal listener semantics
+#' and pragmatic judgments for four alternatives scalar items from
+#' Peloquin & Frank 2016.
+#'
+#' 1) quantityVarName :: "stars"
+#'
+#' 2) semanticsVarName :: "speaker.p"
+#'
+#' 3) itemVarName :: "words"
+#'
+#' 4) groupVarName :: "scale"
+#'
+#' other) experiment number :: "exp"
+#'
+#' other) Pragmatic judgments study 1 (e6), study 2 (e11)
+#'
 #' @name peloquinFrank_4Alts
 #' @docType data
 #' @keywords data
+#' @references \url{https://github.com/langcog/scalar_implicature}
 #'
 NULL
