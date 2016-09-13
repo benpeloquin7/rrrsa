efac2e2a3aea396a6b1be5d11a44423bd77e43d3
benpeloquin7 <benjamin.peloquin@gmail.com>
2016-09-13 11:55:34 -0700

    adding pragmods data, need to changedocumentation

---
+++ R/d_pragmods.R
@@ -0,0 +1,22 @@
+#' Data from Rational speech act models of pragmatic reasoning in reference games
+#'
+#' This data set contains ....
+#'
+#' 1) quantityVarName :: "stars"
+#'
+#' 2) semanticsVarName :: "speaker.p"
+#'
+#' 3) itemVarName :: "words"
+#'
+#' 4) groupVarName :: "scale"
+#'
+#' other) experiment number :: "exp"
+#'
+#' other) Pragmatic judgments study 1 (e6), study 2 (e11)
+#'
+#' @name d_pragmods
+#' @docType data
+#' @keywords data
+#' @references \url{https://github.com/langcog/scalar_implicature}
+#'
+NULL
